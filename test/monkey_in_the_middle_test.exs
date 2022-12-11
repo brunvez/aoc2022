37b170815d1818392727b72488a84fb785ea7b55
Bruno Vezoli <brunvez@gmail.com>
2022-12-11 21:27:47 +0700

    Day 11

---
+++ test/monkey_in_the_middle_test.exs
@@ -0,0 +1,19 @@
+defmodule AOC2022.MonkeyInTheMiddleTest do
+  use ExUnit.Case
+
+  alias AOC2022.Input
+  alias AOC2022.MonkeyInTheMiddle
+
+  test "returns the level of monkey businesses after 20 rounds" do
+    input = Input.read("monkey_in_the_middle.txt")
+
+    assert MonkeyInTheMiddle.solve(input, worry_divisor: 3, rounds: 20) == 118_674
+  end
+
+  @tag timeout: :infinity
+  test "returns the level of monkey businesses after 10_000 rounds with no worry divisor" do
+    input = Input.read("monkey_in_the_middle.txt")
+
+    assert MonkeyInTheMiddle.solve(input, worry_divisor: 1, rounds: 10_000) == 32_333_418_600
+  end
+end
